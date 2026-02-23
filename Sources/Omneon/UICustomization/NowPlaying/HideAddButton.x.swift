e41ab78efb2fcd8ef096bee35b02b597b6b5d79f
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-23 13:49:39 -0600

    Update HideAddButton.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideAddButton.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideAddButton.x.swift
@@ -4,15 +4,16 @@ import SwiftUI
 
 struct HideAddButton: HookGroup { }
 
-class NowPlaying_ModesImpl_InformationElementsUnit_Hook_2: ClassHook<UIView> {
+class NowPlaying_ModesImpl_InformationElementsUnit_Hook_2: ClassHook<UIViewController> {
     typealias Group = HideAddButton
     static let targetName = "NowPlaying_ModesImpl.InformationElementsUnit"
 
     func viewDidLayoutSubviews() {
         orig.viewDidLayoutSubviews()
 
-        let root = target
-        guard root.subviews.count > 1 else { return }
+        let root = target.view
+
+        guard let root = root, root.subviews.count > 1 else { return }
 
         let v = root.subviews[1]
 
@@ -20,6 +21,7 @@ class NowPlaying_ModesImpl_InformationElementsUnit_Hook_2: ClassHook<UIView> {
             v.isHidden = true
             return
         }
+
         if stack.arrangedSubviews.count > 1 {
             let second = stack.arrangedSubviews[1]
             stack.removeArrangedSubview(second)
