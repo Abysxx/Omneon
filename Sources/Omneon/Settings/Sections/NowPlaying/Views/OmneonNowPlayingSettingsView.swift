f8a9b47cfabadc8bc901d5bd613bf9c589f5bce5
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-23 16:01:31 -0600

    Update OmneonNowPlayingSettingsView.swift

--- Sources/Omneon/Settings/Sections/NowPlaying/Views/OmneonNowPlayingSettingsView.swift
+++ Sources/Omneon/Settings/Sections/NowPlaying/Views/OmneonNowPlayingSettingsView.swift
@@ -22,6 +22,21 @@ struct OmneonNowPlayingSettingsView: View {
                         set: { UserDefaults.hideBanButton = $0 }
                     )
                 )
+                
+                Toggle(
+                    "hide_explore".localized,
+                    isOn: Binding<Bool>(
+                        get: { UserDefaults.hideExplore },
+                        set: { UserDefaults.hideExplore = $0 }
+                    )
+                )
+                Toggle(
+                    "hide_credits".localized,
+                    isOn: Binding<Bool>(
+                        get: { UserDefaults.hideCredits },
+                        set: { UserDefaults.hideCredits = $0 }
+                    )
+                )
             }
 
             NonIPadSpacerView()
