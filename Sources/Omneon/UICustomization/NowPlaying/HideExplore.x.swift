82e372ba51db3eb08bb3330b607a0c7e5bebdffb
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 11:12:45 -0600

    Update HideExplore.x.swift
    uhhuh..

--- Sources/Omneon/UICustomization/NowPlaying/HideExplore.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideExplore.x.swift
@@ -6,28 +6,48 @@ struct HideExplore: HookGroup { }
 class ScrollCollectionViewHook_0: ClassHook<UICollectionView> {
     typealias Group = HideExplore
     static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewWithDynamicSizing"
+    
+    private var hiddenIndexPaths = Set<IndexPath>()
 
     func layoutSubviews() {
         orig.layoutSubviews()
-        
+
         let targetIdentifier = "Components.UI.ArtistBioCardNowPlayingView"
-    
+
         for cell in target.visibleCells {
-            // Probably unstable
-            if cell.subviews[0].subviews[0].subviews[0].accessibilityIdentifier == targetIdentifier {
+            guard let firstSubview = cell.subviews.first,
+                  let secondSubview = firstSubview.subviews.first,
+                  let thirdSubview = secondSubview.subviews.first else { continue }
+
+            if thirdSubview.accessibilityIdentifier == targetIdentifier {
                 cell.alpha = 0
                 cell.isUserInteractionEnabled = false
                 cell.isHidden = true
                 cell.contentView.isHidden = true
-                cell.setNeedsLayout()
-                cell.layoutIfNeeded()
-                /*if let indexPath = target.indexPath(for: cell),
-                   let attributes = target.layoutAttributesForItem(at: indexPath) {
 
-                    attributes.size = .zero
-                    attributes.frame.size = .zero
-                }*/
+                // Remember indexPath for layout adjustment
+                if let indexPath = target.indexPath(for: cell) {
+                    hiddenIndexPaths.insert(indexPath)
+                }
             }
         }
+        target.collectionViewLayout.invalidateLayout()
+    }
+}
+
+// MARK: - Override layout attributes to hide cells fully
+extension ScrollCollectionViewHook_0 {
+
+    func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let attrs = orig.layoutAttributesForItem(at: indexPath) else { return nil }
+
+        if hiddenIndexPaths.contains(indexPath) {
+            attrs.size = .zero
+            attrs.frame.size = .zero
+            attrs.alpha = 0
+            attrs.isHidden = true
+        }
+
+        return attrs
     }
 }
