7b199d74e75710860e3c78747750fad834363545
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 12:35:35 -0600

    Update HideExplore.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideExplore.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideExplore.x.swift
@@ -15,22 +15,9 @@ class ScrollCollectionViewHook_0: ClassHook<UICollectionView> {
         
         for cell in target.visibleCells {
             if cell.subviews[0].subviews[0].subviews[0].accessibilityIdentifier == targetIdentifier {
-                // Hide the cell
-                cell.isHidden = true
-                cell.alpha = 0
-                cell.isUserInteractionEnabled = false
-                cell.contentView.isHidden = true
-                
-                // Force UICollectionView to treat it as zero size
-                if let indexPath = target.indexPath(for: cell),
-                   let layout = target.collectionViewLayout as? UICollectionViewFlowLayout {
-                    
-                    // Use invalidation to force the layout to recalc
-                    layout.invalidateLayout(with: UICollectionViewFlowLayoutInvalidationContext())
-                    
-                    // Optional: if using iOS 14+ self-sizing cells
+                if let indexPath = target.indexPath(for: cell) {
                     target.performBatchUpdates({
-                        target.reloadItems(at: [indexPath])
+                        target.deleteItems(at: [indexPath])
                     }, completion: nil)
                 }
             }
