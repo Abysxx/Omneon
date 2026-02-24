0552b3a929c5914db42015e3078945afa55217c5
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 09:27:37 -0600

    Update HideCredits.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
@@ -10,10 +10,13 @@ class ScrollCollectionViewHook: ClassHook<UICollectionView> {
     func layoutSubviews() {
         orig.layoutSubviews()
 
-        let targetIdentifier = "scrolling_npv_collection_view_cell_accessibility_identifier_2"
-
+        // So apparently this changes and isn't the same everytime smh
+        //let targetIdentifier = "scrolling_npv_collection_view_cell_accessibility_identifier_2"
+        let targetIdentifier = "TrackCredits.Card"
+    
         for cell in target.visibleCells {
-            if cell.accessibilityIdentifier == targetIdentifier {
+            // Probably unstable
+            if cell.subviews[0].subviews[0].accessibilityIdentifier == targetIdentifier {
                 cell.alpha = 0
                 cell.isUserInteractionEnabled = false
                 if let indexPath = target.indexPath(for: cell),
