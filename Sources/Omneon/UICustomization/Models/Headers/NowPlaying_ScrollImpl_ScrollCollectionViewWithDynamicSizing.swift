f8c3c0824cf0927b85e29a671806fd295f6c36fc
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 10:01:21 -0600

    Delete Sources/Omneon/UICustomization/Models/Headers/NowPlaying_ScrollImpl_ScrollCollectionViewWithDynamicSizing.swift

--- Sources/Omneon/UICustomization/Models/Headers/NowPlaying_ScrollImpl_ScrollCollectionViewWithDynamicSizing.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-@objc protocol NowPlaying_ScrollImpl_ScrollCollectionViewWithDynamicSizing {
-    @objc(reloadItemsAtIndexPaths:)
-    func reloadItems(at indexPaths: [IndexPath])
-}
