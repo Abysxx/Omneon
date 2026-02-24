f41438fd96962ed1546ba6e6b9123985bab24b18
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 12:17:31 -0600

    Update NowPlaying_ScrollImpl.swift

--- Sources/Omneon/UICustomization/Models/Headers/NowPlaying_ScrollImpl.swift
+++ Sources/Omneon/UICustomization/Models/Headers/NowPlaying_ScrollImpl.swift
@@ -2,13 +2,11 @@ import UIKit
 import Foundation
 
 @objc protocol ScrollCollectionViewWithDynamicSizingProtocol {
-    @objc optional func reloadItemsAtIndexPaths(_ indexPaths: [IndexPath])
-    @objc optional func insertItemsAtIndexPaths(_ indexPaths: [IndexPath])
-    @objc optional func deleteItemsAtIndexPaths(_ indexPaths: [IndexPath])
+    @objc optional func reloadItems(at indexPaths: [IndexPath])
+    @objc optional func insertItems(at indexPaths: [IndexPath])
+    @objc optional func deleteItems(at indexPaths: [IndexPath])
     @objc optional func layoutSubviews()
     @objc optional var visibleCells: [UICollectionViewCell] { get }
-    @objc optional func indexPathForCell(_ cell: UICollectionViewCell) -> IndexPath?
-    @objc optional func layoutAttributesForItemAtIndexPath(_ indexPath: IndexPath) -> UICollectionViewLayoutAttributes?
+    @objc optional func indexPath(for cell: UICollectionViewCell) -> IndexPath?
+    @objc optional func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes?
 }
-
-@objc class ScrollCollectionViewWithDynamicSizing: UICollectionView, ScrollCollectionViewWithDynamicSizingProtocol { }
