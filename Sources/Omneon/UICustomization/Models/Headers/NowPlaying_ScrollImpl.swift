c54d94aaa5c9fddfcf4379a140681b8aa36bcd28
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 11:52:29 -0600

    Create NowPlaying_ScrollImpl.swift

---
+++ Sources/Omneon/UICustomization/Models/Headers/NowPlaying_ScrollImpl.swift
@@ -0,0 +1,16 @@
+import UIKit
+import foundation
+
+// MARK: - Protocol for hooking ScrollCollectionViewWithDynamicSizing
+@objc protocol ScrollCollectionViewWithDynamicSizingProtocol {
+    @objc optional func insertItems(at indexPaths: [IndexPath])
+    @objc optional func deleteItems(at indexPaths: [IndexPath])
+    @objc optional func reloadItems(at indexPaths: [IndexPath])
+    @objc optional func layoutSubviews()
+    @objc optional var visibleCells: [UICollectionViewCell] { get }
+    @objc optional func indexPath(for cell: UICollectionViewCell) -> IndexPath?
+    @objc optional func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes?
+}
+
+// MARK: - Target class
+@objc class ScrollCollectionViewWithDynamicSizing: UICollectionView, ScrollCollectionViewWithDynamicSizingProtocol { }
