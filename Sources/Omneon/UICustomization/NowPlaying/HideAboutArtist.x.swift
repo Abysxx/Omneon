3580e541659155fc3967a97fa5b5cef30e2f6784
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-24 08:39:25 -0600

    Create HideAboutArtist.x.swift

---
+++ Sources/Omneon/UICustomization/NowPlaying/HideAboutArtist.x.swift
@@ -0,0 +1,27 @@
+import Orion
+import UIKit
+
+struct HideCredits: HookGroup { }
+
+class ScrollCollectionViewHook_3: ClassHook<UICollectionView> {
+    typealias Group = HideCredits
+    static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewWithDynamicSizing"
+
+    func layoutSubviews() {
+        orig.layoutSubviews()
+
+        let targetIdentifier = "scrolling_npv_collection_view_cell_accessibility_identifier_1"
+
+        for cell in target.visibleCells {
+            if cell.accessibilityIdentifier == targetIdentifier {
+                NSLog("[Fire] Found target cell, hiding it.")
+                cell.isHidden = true
+                
+                // If you prefer full removal instead:
+                //if let indexPath = target.indexPath(for: cell) {
+                //     target.deleteItems(at: [indexPath])
+                // }
+            }
+        }
+    }
+}
