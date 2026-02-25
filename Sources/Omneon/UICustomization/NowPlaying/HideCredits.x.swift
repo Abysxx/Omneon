f7c7f9715143fbecc34900b2b46afadc1352ecf0
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-25 13:27:24 -0600

    Update HideCredits.x.swift
    trying something a bit different

--- Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
@@ -4,52 +4,27 @@ import Foundation
 
 struct HideCredits: HookGroup {}
 
-// Track the index path to hide
-var hiddenIndexPath_2: IndexPath? = nil
-
-class HideCredits_CollectionViewHook: ClassHook<UICollectionView> {
-    typealias Group = HideCredits
-    static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewWithDynamicSizing"
-
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        
-        for cell in target.visibleCells {
-            if cell.subviews.contains(where: { subview in
-                subview.subviews.contains(where: { $0.accessibilityIdentifier == "TrackCredits.Card" })
-            }) {
-                if let indexPath = target.indexPath(for: cell) {
-                    if hiddenIndexPath_2 != indexPath {
-                        hiddenIndexPath_2 = indexPath
-                        target.collectionViewLayout.invalidateLayout()
-                    }
-                }
-                cell.isHidden = true
-                cell.isUserInteractionEnabled = false
-                break
-            }
-        }
-    }
-}
-
 class HideCredits_DelegateHook: ClassHook<NSObject> {
     typealias Group = HideCredits
     static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewManagerWithDynamicSizingImplementation"
 
-    @objc(collectionView:numberOfItemsInSection:)
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
-        return hiddenIndexPath_2 != nil ? count - 1 : count
-    }
-
     @objc(collectionView:cellForItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let hidden = hiddenIndexPath_2 else {
-            return orig.collectionView(collectionView, cellForItemAt: indexPath)
+        let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
+        if findAccessibilityIdentifier(cell, identifier: "TrackCredits.Card") {
+            HideCreditsState.hiddenIndexPath = indexPath
+            collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "HideCredits_EmptyCell")
+            return collectionView.dequeueReusableCell(withReuseIdentifier: "HideCredits_EmptyCell", for: indexPath)
         }
-        let adjustedIndexPath = indexPath.item >= hidden.item
-            ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
-            : indexPath
-        return orig.collectionView(collectionView, cellForItemAt: adjustedIndexPath)
+        return cell
+    }
+
+    @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath == HideCreditsState.hiddenIndexPath {
+            cell.frame = .zero
+            cell.isHidden = true
+        }
+        orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
     }
 }
