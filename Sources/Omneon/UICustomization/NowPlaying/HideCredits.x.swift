7581789962e65677542b9e8106bc830de0c3d306
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-25 16:27:43 -0600

    Update HideCredits.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
@@ -4,63 +4,20 @@ import Foundation
 
 struct HideCredits: HookGroup {}
 
-var hiddenIndexPath_2: IndexPath? = nil
-var hiddenCellHeight_2: CGFloat = 0
-
-class HideCredits_DelegateHook: ClassHook<NSObject> {
+class HideCredits_CellHook: ClassHook<UIView> {
     typealias Group = HideCredits
-    static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewManagerWithDynamicSizingImplementation"
+    static let targetName = "NowPlaying_ScrollImpl.NowPlayingScrollCellWithDynamicSizing"
     
-    @objc(collectionView:cellForItemAtIndexPath:)
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // Overwrite the original registration once
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "TRACK_CREDITS")
-        
-        if indexPath == hiddenIndexPath_2 {
-            return collectionView.dequeueReusableCell(withReuseIdentifier: "TRACK_CREDITS", for: indexPath)
+    func didMoveToSuperview() {
+        orig.didMoveToSuperview()
+        if containsIdentifier(target, identifier: "TrackCredits.Card") {
+            target.isHidden = true
+            target.frame = .zero
+            // Also constrain height to 0
+            target.constraints.filter { $0.firstAttribute == .height }.forEach { $0.isActive = false }
+            let zero = target.heightAnchor.constraint(equalToConstant: 0)
+            zero.priority = .required
+            zero.isActive = true
         }
-        
-        let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
-        if containsIdentifier(cell, identifier: "TrackCredits.Card") {
-            hiddenIndexPath_2 = indexPath
-            return collectionView.dequeueReusableCell(withReuseIdentifier: "TRACK_CREDITS", for: indexPath)
-        }
-        return cell
-    }
-    
-    @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if indexPath == hiddenIndexPath_2 {
-            cell.layoutIfNeeded()
-            hiddenCellHeight_2 = cell.bounds.height
-            NSLog("[Omneon] Cell Height: \(cell.bounds.height)")
-            cell.isHidden = true
-    
-            // Remove any previously added zero height constraints to avoid duplicates
-            cell.constraints.filter { $0.identifier == "TRACK_CREDITS" }.forEach { $0.isActive = false }
-            cell.contentView.constraints.forEach { $0.isActive = $0.firstAttribute != .height }
-            cell.constraints.forEach { $0.isActive = $0.firstAttribute != .height }
-    
-            let zeroHeight = cell.heightAnchor.constraint(equalToConstant: 0)
-            zeroHeight.priority = .required
-            zeroHeight.identifier = "TRACK_CREDITS"
-            zeroHeight.isActive = true
-            cell.layoutIfNeeded()
-    
-            // Invalidate layout to pick up correct height
-            DispatchQueue.main.async {
-                collectionView.collectionViewLayout.invalidateLayout()
-            }
-        }
-        orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
-    }
-    
-    @objc(collectionView:layout:insetForSectionAtIndex:)
-    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        var insets = orig.collectionView(collectionView, layout: layout, insetForSectionAt: section)
-        if hiddenIndexPath_2 != nil && hiddenCellHeight_2 > 0 {
-            insets.bottom -= hiddenCellHeight_2
-        }
-        return insets
     }
 }
