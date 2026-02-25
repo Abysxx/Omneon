acec13a286220c7ccdaeb6d996275649edb82ba2
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-25 08:27:57 -0600

    Update HideAboutArtist.x.swift
    changed it back to accessibilityIdentifier and fixed misnamed hiddenindexpath

--- Sources/Omneon/UICustomization/NowPlaying/HideAboutArtist.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideAboutArtist.x.swift
@@ -14,17 +14,13 @@ class HideAboutArtist_CollectionViewHook: ClassHook<UICollectionView> {
     func layoutSubviews() {
         orig.layoutSubviews()
         
-        // Such a long name bruh
-        let targetClass: AnyClass = NSClassFromString("_TtC14Creator_ECMKitP33_9A9A9C9886A2DEDE51521D11F82E7F9026CreatorBiographyCardLayout")!
-
         for cell in target.visibleCells {
             if cell.subviews.contains(where: { subview in
-                subview.subviews.contains(where: { $0.isKind(of: targetClass) })
+                subview.subviews.contains(where: { $0.accessibilityIdentifier == "Components.UI.ArtistBioCardNowPlayingView" })
             }) {
                 if let indexPath = target.indexPath(for: cell) {
-                    // Only invalidate if the hidden path changed
-                    if hiddenIndexPath_1 != indexPath {
-                        hiddenIndexPath_1 = indexPath
+                    if hiddenIndexPath_3 != indexPath {
+                        hiddenIndexPath_3 = indexPath
                         target.collectionViewLayout.invalidateLayout()
                     }
                 }
