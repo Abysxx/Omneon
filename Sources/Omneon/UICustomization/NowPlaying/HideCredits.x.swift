42272161900e2c95ac4b62237e2ee9b37f29ee4f
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-25 13:53:46 -0600

    Update HideCredits.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
@@ -24,9 +24,34 @@ class HideCredits_DelegateHook: ClassHook<NSObject> {
     @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         if indexPath == hiddenIndexPath_2 {
-            cell.frame = .zero
             cell.isHidden = true
+            cell.contentView.constraints.forEach { constraint in
+                if constraint.firstAttribute == .height {
+                    constraint.isActive = false
+                }
+            }
+            cell.constraints.forEach { constraint in
+                if constraint.firstAttribute == .height {
+                    constraint.isActive = false
+                }
+            }
+            let zeroHeight = cell.heightAnchor.constraint(equalToConstant: 0)
+            zeroHeight.priority = .required
+            zeroHeight.isActive = true
+            cell.layoutIfNeeded()
         }
         orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
+    }
+
+    @objc(collectionView:layout:insetForSectionAtIndex:)
+    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        var insets = orig.collectionView(collectionView, layout: layout, insetForSectionAt: section)
+        NSLog("[Omneon] itemSize: \((layout as? UICollectionViewFlowLayout)?.itemSize ?? .zero)")
+        NSLog("[Omneon] insets: \(insets)")
+        if hiddenIndexPath_2 != nil {
+            let itemSize = (layout as? UICollectionViewFlowLayout)?.itemSize ?? .zero
+            insets.bottom -= itemSize.height
+        }
+        return insets
     }
 }
