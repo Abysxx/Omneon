f0085c6901e8063a656838339d5134e2b90b830c
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-25 12:14:12 -0600

    Update HideCredits.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
@@ -61,13 +61,11 @@ class HideCredits_LayoutHook: ClassHook<UICollectionViewLayout> {
         return attrs as NSArray
     }
 
-    @objc(collectionView:layout:sizeForItemAtIndexPath:)
-    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if indexPath == hiddenIndexPath_2 {
-            NSLog("[Omneon] CollectionView Attempt")
-            return .zero
-        }
-        return orig.collectionView(collectionView, layout: layout, sizeForItemAt: indexPath)
+    @objc(collectionView:numberOfItemsInSection:)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        NSLog("[Omneon] Yup")
+        let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
+        return HideExploreState.hiddenIndexPath != nil ? count - 1 : count
     }
 
     @objc(layoutAttributesForItemAtIndexPath:)
