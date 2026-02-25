e6e10236894106d141a5e86362968c4157c37311
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-25 16:34:08 -0600

    Update HideCredits.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideCredits.x.swift
@@ -8,16 +8,17 @@ class HideCredits_CellHook: ClassHook<UIView> {
     typealias Group = HideCredits
     static let targetName = "NowPlaying_ScrollImpl.NowPlayingScrollCellWithDynamicSizing"
     
-    func didMoveToSuperview() {
-        orig.didMoveToSuperview()
+    func systemLayoutSizeFitting(_ targetSize: CGSize) -> CGSize {
         if containsIdentifier(target, identifier: "TrackCredits.Card") {
-            target.isHidden = true
-            target.frame = .zero
-            // Also constrain height to 0
-            target.constraints.filter { $0.firstAttribute == .height }.forEach { $0.isActive = false }
-            let zero = target.heightAnchor.constraint(equalToConstant: 0)
-            zero.priority = .required
-            zero.isActive = true
+            return .zero
         }
+        return orig.systemLayoutSizeFitting(targetSize)
+    }
+    
+    func systemLayoutSizeFitting(_ targetSize: CGSize, withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority, verticalFittingPriority: UILayoutPriority) -> CGSize {
+        if containsIdentifier(target, identifier: "TrackCredits.Card") {
+            return .zero
+        }
+        return orig.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: horizontalFittingPriority, verticalFittingPriority: verticalFittingPriority)
     }
 }
