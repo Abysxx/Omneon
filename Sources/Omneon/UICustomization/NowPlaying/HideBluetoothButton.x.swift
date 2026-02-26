1098119bc587b2edeeb2d96356f5621495efbd26
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-26 13:54:03 -0600

    Update HideBluetoothButton.x.swift

--- Sources/Omneon/UICustomization/NowPlaying/HideBluetoothButton.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideBluetoothButton.x.swift
@@ -2,28 +2,29 @@ import Orion
 import UIKit
 import SwiftUI
 
+
 struct HideBluetoothButton: HookGroup { }
 
-class NowPlaying_ModesImpl_InformationElementsUnit_Hook_123: ClassHook<UIViewController> {
-    typealias Group = HideBanButton
-    static let targetName = "NowPlaying_ModesImpl.InformationElementsUnit"
+class NowPlaying_ModesImpl_FooterElementsUnit_Hook_1: ClassHook<UIViewController> {
+    typealias Group = HideBluetoothButton
+    static let targetName = "NowPlaying_ModesImpl.FooterElementsUnit"
     
     private let hideIndex: Int = 0
 
     func viewDidLayoutSubviews() {
         orig.viewDidLayoutSubviews()
-
+        // I think this still works works?
         guard let root = target.view, root.subviews.count > 1 else { return }
         let container = root.subviews[1]
 
         guard let stack = container as? UIStackView else {
-            container.isHidden = true
+            container.alpha = 0
             return
         }
 
         if stack.arrangedSubviews.count > hideIndex {
             let viewToHide = stack.arrangedSubviews[hideIndex]
-            viewToHide.isHidden = true
+            viewToHide.alpha = 0
         }
     }
 }
