a879f1d3b8c767e0110c7a4e07064db47cd1dc8d
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-26 12:53:42 -0600

    Update HideAboutArtist.x.swift
    Finally?

--- Sources/Omneon/UICustomization/NowPlaying/HideAboutArtist.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideAboutArtist.x.swift
@@ -4,80 +4,86 @@ import Foundation
 
 struct HideAboutArtist: HookGroup {}
 
-// Track the index path to hide
 var hiddenIndexPath_3: IndexPath? = nil
 
-func containsIdentifier(_ view: UIView, identifier: String) -> Bool {
-    if view.accessibilityIdentifier == identifier {
-        return true
-    }
-    for subview in view.subviews {
-        if containsIdentifier(subview, identifier: identifier) {
-            return true
+func scanAndHideBiography(in collectionView: UICollectionView) {
+    for cell in collectionView.visibleCells {
+        if containsIdentifier(cell, identifier: "Components.UI.ArtistBioCardNowPlayingView"),
+           let indexPath = collectionView.indexPath(for: cell) {
+            if hiddenIndexPath_3 != indexPath {
+                hiddenIndexPath_3 = indexPath
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
+                }
+            }
+            break
         }
     }
-    return false
 }
 
-class HideAboutArtist_CollectionViewHook: ClassHook<UICollectionView> {
+class HideAboutArtist_ViewControllerHook: ClassHook<UIViewController> {
     typealias Group = HideAboutArtist
-    static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewWithDynamicSizing"
+    static let targetName = "NowPlaying_ScrollImpl.NowPlayingScrollViewController"
 
-    
-    
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        for cell in target.visibleCells {
+    @objc(nowPlayingScrollViewModelWithDidLoadComponentsFor:withDifferentProviders:scrollEnabledValueChanged:)
+    func nowPlayingScrollViewModelWithDidLoadComponents(for arg1: AnyObject, withDifferentProviders arg2: Bool, scrollEnabledValueChanged arg3: Bool) {
+        orig.nowPlayingScrollViewModelWithDidLoadComponents(for: arg1, withDifferentProviders: arg2, scrollEnabledValueChanged: arg3)
+        guard let collectionView = target.value(forKey: "collectionView") as? UICollectionView else {
+            return
+        }
+        scanAndHideBiography(in: collectionView)
+    }
+
+    @objc(nowPlayingScrollViewModelWithDidMoveToRelativeTrack:withDifferentProviders:scrollEnabledValueChanged:)
+    func nowPlayingScrollViewModelWithDidMoveToRelativeTrack(for arg1: AnyObject, withDifferentProviders arg2: Bool, scrollEnabledValueChanged arg3: Bool) {
+        orig.nowPlayingScrollViewModelWithDidMoveToRelativeTrack(for: arg1, withDifferentProviders: arg2, scrollEnabledValueChanged: arg3)
+        guard let collectionView = target.value(forKey: "collectionView") as? UICollectionView else {
+            return
+        }
+        hiddenIndexPath_3 = nil
+        scanAndHideBiography(in: collectionView)
+    }
+}
+
+class HideAboutArtist_DelegateHook: ClassHook<NSObject> {
+    typealias Group = HideAboutArtist
+    static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewManagerWithDynamicSizingImplementation"
+
+    @objc(collectionView:numberOfItemsInSection:)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
+        let adjusted = hiddenIndexPath_3 != nil ? count - 1 : count
+        return adjusted
+    }
+
+    @objc(collectionView:cellForItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let hidden = hiddenIndexPath_3 else {
+            // Haven't found the target yet, check this cell
+            let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
             if containsIdentifier(cell, identifier: "Components.UI.ArtistBioCardNowPlayingView") {
-            if let indexPath = target.indexPath(for: cell) {
-                    if hiddenIndexPath_3 != indexPath {
-                        hiddenIndexPath_3 = indexPath
-                        target.collectionViewLayout.invalidateLayout()
-                    }
+                hiddenIndexPath_3 = indexPath
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
                 }
-                cell.isHidden = true
-                cell.isUserInteractionEnabled = false
-                
+            }
+            return cell
+        }
+        let adjustedIndexPath = indexPath.item >= hidden.item
+            ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
+            : indexPath
+        return orig.collectionView(collectionView, cellForItemAt: adjustedIndexPath)
+    }
+
+    @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        // Catch cells that had subviews added asynchronously
+        if hiddenIndexPath_3 == nil && containsIdentifier(cell, identifier: "Components.UI.ArtistBioCardNowPlayingView") {
+            hiddenIndexPath_3 = indexPath
+            DispatchQueue.main.async {
+                collectionView.reloadData()
             }
         }
-    }
-}
-
-class HideAboutArtist_LayoutHook: ClassHook<UICollectionViewLayout> {
-    typealias Group = HideAboutArtist
-
-    // Match the layout used by the target collection view
-    static let targetName = "NowPlaying_ScrollImpl.NowPlayingScrollLayout"
-
-    @objc(layoutAttributesForElementsInRect:)
-    func layoutAttributesForElements(in rect: CGRect) -> NSArray? {
-        guard var attrs = orig.layoutAttributesForElements(in: rect) as? [UICollectionViewLayoutAttributes] else { return nil }
-        if let hidden = hiddenIndexPath_3 {
-            attrs = attrs.map { attr in
-                // Only target cells, not headers/footers/decorations
-                if attr.representedElementCategory == .cell && attr.indexPath == hidden {
-                    let zeroed = attr.copy() as! UICollectionViewLayoutAttributes
-                    zeroed.frame = .zero
-                    zeroed.isHidden = true
-                    return zeroed
-                }
-                return attr
-            }
-        }
-        return attrs as NSArray
-    }
-    
-
-    @objc(layoutAttributesForItemAtIndexPath:)
-    func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        guard let attr = orig.layoutAttributesForItem(at: indexPath) else { return nil }
-
-        if let hidden = hiddenIndexPath_3, indexPath == hidden {
-            let zeroed = attr.copy() as! UICollectionViewLayoutAttributes
-            zeroed.frame = .zero
-            zeroed.isHidden = true
-            return zeroed
-        }
-        return attr
+        orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
     }
 }
