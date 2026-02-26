06cadc54ba7430990fcb08131453cb77a78e07b1
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-26 13:57:51 -0600

    Create HideQueueButton.x.swift

---
+++ Sources/Omneon/UICustomization/NowPlaying/HideQueueButton.x.swift
@@ -0,0 +1,30 @@
+import Orion
+import UIKit
+import SwiftUI
+
+
+struct HideQueueButton: HookGroup { }
+
+class NowPlaying_ModesImpl_FooterElementsUnit_Hook_3: ClassHook<UIViewController> {
+    typealias Group = HideBluetoothButton
+    static let targetName = "NowPlaying_ModesImpl.FooterElementsUnit"
+    
+    private let hideIndex: Int = 5
+
+    func viewDidLayoutSubviews() {
+        orig.viewDidLayoutSubviews()
+      
+        guard let root = target.view, root.subviews.count > 1 else { return }
+        let container = root.subviews[1]
+
+        guard let stack = container as? UIStackView else {
+            container.alpha = 0
+            return
+        }
+
+        if stack.arrangedSubviews.count > hideIndex {
+            let viewToHide = stack.arrangedSubviews[hideIndex]
+            viewToHide.alpha = 0
+        }
+    }
+}
