d161bc65b67913c8f9ee55acd5beaf70bf8dcc0f
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-27 12:11:54 -0600

    Create CenterTitle.x.swift

---
+++ Sources/Omneon/UICustomization/NowPlaying/CenterTitle.x.swift
@@ -0,0 +1,23 @@
+import Orion
+import UIKit
+import SwiftUI
+
+struct CenterTitle: HookGroup { }
+
+class NowPlaying_BarImpl_ContentViewControllerImplementation_Hook_2: ClassHook<UIViewController> {
+    typealias Group = CenterTitle
+    static let targetName = "NowPlaying_BarImpl.ContentViewControllerImplementation"
+    
+    private let hideIndex: Int = 3
+
+    func viewDidLayoutSubviews() {
+        orig.viewDidLayoutSubviews()
+
+        guard let root = target.view, root.subviews.count >= 5 else { return }
+        let viewToCenter = root.subviews[hideIndex]
+        // shift the frame down 8 on the x-axis
+        var frame = viewToCenter.frame
+        frame.origin.x = 8
+        viewToCenter.frame = frame
+    }
+}
