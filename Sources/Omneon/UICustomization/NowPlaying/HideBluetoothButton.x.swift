dab2c92a219adeac931ed1381f049b794ef8977a
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-02-27 09:48:22 -0600

    Update HideBluetoothButton.x.swift
    removed logging

--- Sources/Omneon/UICustomization/NowPlaying/HideBluetoothButton.x.swift
+++ Sources/Omneon/UICustomization/NowPlaying/HideBluetoothButton.x.swift
@@ -13,20 +13,15 @@ class NowPlaying_ModesImpl_FooterElementsUnit_Hook_1: ClassHook<UIViewController
 
     func viewDidLayoutSubviews() {
         orig.viewDidLayoutSubviews()
-        NSLog("[Omneon] SubviewCount: \(target.view.subviews.count)")
         guard let root = target.view, root.subviews.count >= 1 else { return }
         let container = root.subviews[0]
-        NSLog("[Omneon]1 Container: \(container)")
         guard let stack = container as? UIStackView else {
-            NSLog("[Omneon]1 Setting Container.aplha=0")
             container.alpha = 0
             return
         }
 
         if stack.arrangedSubviews.count > hideIndex {
-            NSLog("[Omneon]1 Setting viewToHide.aplha=0")
             let viewToHide = stack.arrangedSubviews[hideIndex]
-            NSLog("[Omneon]1 ViewToHide: \(viewToHide)")
             viewToHide.alpha = 0
         }
     }
