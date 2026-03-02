b26a74ae5b908bdd60e10956b5db42555ad012b1
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-02 09:28:02 -0600

    Create OmneonYourLibrarySettingsView.swift

---
+++ Sources/Omneon/Settings/Sections/YourLibrary/Views/OmneonYourLibrarySettingsView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import UIKit
+
+struct OmneonYourLibrarySettingsView: View {
+    var body: some View {
+        List {
+            
+            //Main view (aka when not inside playlist)
+            Section {
+                Toggle(
+                    "force_playlist".localized,
+                    isOn: Binding<Bool>(
+                        get: { UserDefaults.forcePlaylist },
+                        set: { UserDefaults.forcePlaylist = $0 }
+                    )
+                )
+                
+            } header: {
+                Text("main_view".localized)
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+                    .textCase(nil)
+                    .padding(.bottom, 4)
+            }
+
+            NonIPadSpacerView()
+        }
+        .listStyle(GroupedListStyle())
+    }
+}
