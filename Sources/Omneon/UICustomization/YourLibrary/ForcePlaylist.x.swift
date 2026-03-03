5a781b9e94937f1d48ebe6c21e150a2a1f400821
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-03 09:20:56 -0600

    Update ForcePlaylist.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -4,29 +4,48 @@ import SwiftUI
 
 struct ForcePlaylist: HookGroup { }
 
-class YourLibraryViewController_Hook: ClassHook<UIViewController> {
+class YourLibrarySelectedSortOrderImpl_Hook: ClassHook<NSObject> {
     typealias Group = ForcePlaylist
-    static let targetName = "NowPlaying_ModesImpl.InformationElementsUnit"
+    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibrarySelectedSortOrderImpl"
 
     func viewDidLayoutSubviews() {
         orig.viewDidLayoutSubviews()
-        
-        let count = objc_getClassList(nil, 0)
-        var classes = [AnyClass](repeating: NSObject.self, count: Int(count))
-        classes.withUnsafeMutableBufferPointer { buf in
-            objc_getClassList(AutoreleasingUnsafeMutablePointer(buf.baseAddress!), count)
-        }
-        
-        var output = ""
-        for cls in classes {
-            let name = NSStringFromClass(cls)
-            if name.lowercased().contains("collection") || name.lowercased().contains("yourlibrary") {
-                output += "\(name)\n"
+        logIvars()
+    }
+
+    private func logIvars() {
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(type(of: target), &count)
+        for i in 0..<Int(count) {
+            if let ivar = ivars?[i] {
+                let name = String(cString: ivar_getName(ivar)!)
+                let value = object_getIvar(target, ivar)
+                NSLog("[Omneon] YourLibrarySelectedSortOrderImpl ivar \(name): \(String(describing: value))")
             }
         }
-        
-        let path = "/var/jb/var/mobile/Documents/spotify_classes.txt"
-        try? output.write(toFile: path, atomically: true, encoding: .utf8)
-        NSLog("[Omneon] dumped classes to \(path)")
+        free(ivars)
+    }
+}
+
+class CEFilterAndSortRowPresenter_Hook: ClassHook<NSObject> {
+    typealias Group = ForcePlaylist
+    static let targetName = "CollectionEpisodes_PlaylistImpl.CEFilterAndSortRowPresenter"
+
+    func viewDidLayoutSubviews() {
+        orig.viewDidLayoutSubviews()
+        logIvars()
+    }
+
+    private func logIvars() {
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(type(of: target), &count)
+        for i in 0..<Int(count) {
+            if let ivar = ivars?[i] {
+                let name = String(cString: ivar_getName(ivar)!)
+                let value = object_getIvar(target, ivar)
+                NSLog("[Omneon] CEFilterAndSortRowPresenter ivar \(name): \(String(describing: value))")
+            }
+        }
+        free(ivars)
     }
 }
