17e2cb2f2e9e7794142c44d505f8c864e7d2fd61
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-03 09:32:44 -0600

    Update ForcePlaylist.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -7,17 +7,16 @@ struct ForcePlaylist: HookGroup { }
 class YourLibraryViewController_Hook: ClassHook<UIViewController> {
     typealias Group = ForcePlaylist
     static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryViewController"
-
-    func viewDidLayoutSubviews() {
-        orig.viewDidLayoutSubviews()
+    
+    func viewDidLoad() {
+        orig.viewDidLoad()
         var count: UInt32 = 0
-        let ivars = class_copyIvarList(type(of: target), &count)
+        guard let ivars = class_copyIvarList(type(of: target), &count) else { return }
         for i in 0..<Int(count) {
-            if let ivar = ivars?[i] {
-                let name = String(cString: ivar_getName(ivar)!)
-                let value = object_getIvar(target, ivar)
-                NSLog("[Omneon] YourLibraryViewController ivar \(name): \(String(describing: value))")
-            }
+            guard let name = ivar_getName(ivars[i]).map({ String(cString: $0) }) else { continue }
+            let value = object_getIvar(target, ivars[i])
+            guard let value = value else { continue }
+            NSLog("[Omneon] \(name): \(value)")
         }
         free(ivars)
     }
