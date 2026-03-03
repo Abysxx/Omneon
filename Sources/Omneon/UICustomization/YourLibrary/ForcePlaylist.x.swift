b2d01c7134dc9b310c5cc47cb1794958d7d06cec
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-03 09:27:44 -0600

    Update ForcePlaylist.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -4,46 +4,19 @@ import SwiftUI
 
 struct ForcePlaylist: HookGroup { }
 
-class YourLibrarySelectedSortOrderImpl_Hook: ClassHook<NSObject> {
+class YourLibraryViewController_Hook: ClassHook<UIViewController> {
     typealias Group = ForcePlaylist
-    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibrarySelectedSortOrderImpl"
+    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryViewController"
 
     func viewDidLayoutSubviews() {
         orig.viewDidLayoutSubviews()
-        logIvars()
-    }
-
-    private func logIvars() {
         var count: UInt32 = 0
         let ivars = class_copyIvarList(type(of: target), &count)
         for i in 0..<Int(count) {
             if let ivar = ivars?[i] {
                 let name = String(cString: ivar_getName(ivar)!)
                 let value = object_getIvar(target, ivar)
-                NSLog("[Omneon] YourLibrarySelectedSortOrderImpl ivar \(name): \(String(describing: value))")
-            }
-        }
-        free(ivars)
-    }
-}
-
-class CEFilterAndSortRowPresenter_Hook: ClassHook<NSObject> {
-    typealias Group = ForcePlaylist
-    static let targetName = "CollectionEpisodes_PlaylistImpl.CEFilterAndSortRowPresenter"
-
-    func viewDidLayoutSubviews() {
-        orig.viewDidLayoutSubviews()
-        logIvars()
-    }
-
-    private func logIvars() {
-        var count: UInt32 = 0
-        let ivars = class_copyIvarList(type(of: target), &count)
-        for i in 0..<Int(count) {
-            if let ivar = ivars?[i] {
-                let name = String(cString: ivar_getName(ivar)!)
-                let value = object_getIvar(target, ivar)
-                NSLog("[Omneon] CEFilterAndSortRowPresenter ivar \(name): \(String(describing: value))")
+                NSLog("[Omneon] YourLibraryViewController ivar \(name): \(String(describing: value))")
             }
         }
         free(ivars)
