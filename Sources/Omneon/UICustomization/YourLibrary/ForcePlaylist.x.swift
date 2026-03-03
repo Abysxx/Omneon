59555c5a3007b6976c9a7c078ca0981ed9be01b9
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-03 11:40:05 -0600

    Update ForcePlaylist.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -4,33 +4,15 @@ import SwiftUI
 
 struct ForcePlaylist: HookGroup { }
 
-class YourLibraryViewController_Hook: ClassHook<UIViewController> {
+class SPTSortingFilteringUIFactoryImplementation_Hook: ClassHook<NSObject> {
     typealias Group = ForcePlaylist
-    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryViewController"
+    static let targetName = "SPTSortingFilteringUIFactoryImplementation"
 
-    func viewDidAppear(_ animated: Bool) {
-        orig.viewDidAppear(animated)
-        let count = objc_getClassList(nil, 0)
-        var classes = [AnyClass](repeating: NSObject.self, count: Int(count))
-        classes.withUnsafeMutableBufferPointer { buf in
-            objc_getClassList(AutoreleasingUnsafeMutablePointer(buf.baseAddress!), count)
-        }
-        var output = ""
-        for cls in classes {
-            var methodCount: UInt32 = 0
-            let methods = class_copyMethodList(cls, &methodCount)
-            for i in 0..<Int(methodCount) {
-                if let method = methods?[i] {
-                    let sel = NSStringFromSelector(method_getName(method))
-                    if sel.lowercased().contains("sortingfiltering") {
-                        output += "class: \(NSStringFromClass(cls)) method: \(sel)\n"
-                    }
-                }
-            }
-            free(methods)
-        }
-        let path = "/var/jb/var/mobile/Documents/sortingfiltering_dump.txt"
-        try? output.write(toFile: path, atomically: true, encoding: .utf8)
-        NSLog("[Omneon] dumped to \(path)")
+    @objc(createSortingFilteringPickerWithAvailableFilterRules:availableSortRules:selectedFilterRules:selectedSortRule:)
+    func createSortingFilteringPickerWithAvailableFilterRules(_ filterRules: AnyObject, availableSortRules sortRules: AnyObject, selectedFilterRules selectedFilters: AnyObject, selectedSortRule sortRule: AnyObject) -> AnyObject {
+        NSLog("[Omneon] availableFilterRules: \(filterRules)")
+        NSLog("[Omneon] selectedFilterRules: \(selectedFilters)")
+        NSLog("[Omneon] selectedSortRule: \(sortRule)")
+        return orig.createSortingFilteringPickerWithAvailableFilterRules(filterRules, availableSortRules: sortRules, selectedFilterRules: selectedFilters, selectedSortRule: sortRule)
     }
 }
