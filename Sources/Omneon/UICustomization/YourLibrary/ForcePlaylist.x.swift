2ea24557c795310fe831edebc77e070abb2aed16
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-03 11:47:59 -0600

    Update ForcePlaylist.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -4,15 +4,33 @@ import SwiftUI
 
 struct ForcePlaylist: HookGroup { }
 
-class SPTSortingFilteringUIFactoryImplementation_Hook: ClassHook<NSObject> {
+class NowPlaying_ModesImpl_InformationElementsUnit_Hook_2: ClassHook<UIViewController> {
     typealias Group = ForcePlaylist
-    static let targetName = "SPTSortingFilteringUIFactoryImplementation"
-
-    @objc(createSortingFilteringPickerWithAvailableFilterRules:availableSortRules:selectedFilterRules:selectedSortRule:)
-    func createSortingFilteringPickerWithAvailableFilterRules(_ filterRules: AnyObject, availableSortRules sortRules: AnyObject, selectedFilterRules selectedFilters: AnyObject, selectedSortRule sortRule: AnyObject) -> AnyObject {
-        NSLog("[Omneon] availableFilterRules: \(filterRules)")
-        NSLog("[Omneon] selectedFilterRules: \(selectedFilters)")
-        NSLog("[Omneon] selectedSortRule: \(sortRule)")
-        return orig.createSortingFilteringPickerWithAvailableFilterRules(filterRules, availableSortRules: sortRules, selectedFilterRules: selectedFilters, selectedSortRule: sortRule)
+    static let targetName = "NowPlaying_ModesImpl.InformationElementsUnit"
+    
+    func viewDidLayoutSubviews() {
+        orig.viewDidLayoutSubviews()
+        let count = objc_getClassList(nil, 0)
+        var classes = [AnyClass](repeating: NSObject.self, count: Int(count))
+        classes.withUnsafeMutableBufferPointer { buf in
+            objc_getClassList(AutoreleasingUnsafeMutablePointer(buf.baseAddress!), count)
+        }
+        var output = ""
+        for cls in classes {
+            let className = NSStringFromClass(cls)
+            guard className.lowercased().contains("yourlibrary") else { continue }
+            var methodCount: UInt32 = 0
+            let methods = class_copyMethodList(cls, &methodCount)
+            for i in 0..<Int(methodCount) {
+                if let method = methods?[i] {
+                    let sel = NSStringFromSelector(method_getName(method))
+                    output += "\(className) -> \(sel)\n"
+                }
+            }
+            free(methods)
+        }
+        let path = "/var/jb/var/mobile/Documents/yourlibrary_methods.txt"
+        try? output.write(toFile: path, atomically: true, encoding: .utf8)
+        NSLog("[Omneon] dumped to \(path)")
     }
 }
