1d5c75307a19dde8e48d43d89476fc34a76cffe3
Abysxx <159920611+Abysxx@users.noreply.github.com>
2026-03-04 09:57:44 -0600

    Update ForcePlaylist.x.swift
    fixed

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -11,19 +11,21 @@ class YourLibraryViewController_Hook: ClassHook<UIViewController> {
     func viewDidAppear(_ animated: Bool) {
         orig.viewDidAppear(animated)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            guard let filtersView = self.target.view.subviews.first?
-                .subviews.first?
-                .subviews.first?
-                .subviews.first else { return }
-            
-            if let collectionView = filtersView.subviews.first(where: { 
-                $0.accessibilityIdentifier == "Layout.CollectionView" 
-            }) as? UICollectionView {
-                let indexPath = IndexPath(item: 1, section: 0)
-                collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
-                collectionView.delegate?.collectionView?(collectionView, didSelectItemAt: indexPath)
-                NSLog("[Omneon] forced playlist filter selection")
-            }
+            let view = self.target.view
+            guard
+                let s0 = view?.subviews[safe: 0],
+                let s1 = s0.subviews[safe: 1],
+                let s2 = s1.subviews[safe: 1],
+                let s3 = s2.subviews[safe: 0],
+                let s4 = s3.subviews[safe: 0],
+                let s5 = s4.subviews[safe: 0],
+                let collectionView = s5.subviews[safe: 0] as? UICollectionView
+            else { return }
+    
+            NSLog("[Omneon] found collectionView: \(collectionView.accessibilityIdentifier ?? "no id")")
+            let indexPath = IndexPath(item: 1, section: 0)
+            collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+            collectionView.delegate?.collectionView?(collectionView, didSelectItemAt: indexPath)
         }
     }
 }
