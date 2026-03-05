3d0eff1137e50e44fcb222dc0fccc9fdefd49559
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-03-05 14:11:29 -0600

    Update Tweak.x.swift

--- Sources/Omneon/Tweak.x.swift
+++ Sources/Omneon/Tweak.x.swift
@@ -9,7 +9,7 @@ func exitApplication() {
 }
 
 struct Omneon: Tweak {
-    static let version = "0.2.0"
+    static let version = "0.2.1"
     //I don't care about supporting certain app versions. at least right now
     init() {
         //General
