b286c3bc0aa84af8ae931a08b55f9d32323d2817
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-03-05 13:59:53 -0600

    Update ForcePlaylist.x.swift
    i dont need logs

--- Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/ForcePlaylist.x.swift
@@ -29,10 +29,6 @@ class YourLibraryViewController_Hook: ClassHook<UIViewController> {
                 let s5 = s4.subviews[safe: 0],
                 let collectionView = s5.subviews[safe: 0] as? UICollectionView
             else { return }
-
-            NSLog("[Omneon] found collectionView: \(collectionView.accessibilityIdentifier ?? "no id")")
-            NSLog("[Omneon] delegate: \(String(describing: collectionView.delegate))")
-            NSLog("[Omneon] delegate class: \(NSStringFromClass(type(of: collectionView.delegate as AnyObject)))")
             let indexPath = IndexPath(item: 0, section: 0)
             collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
             collectionView.delegate?.collectionView?(collectionView, didSelectItemAt: indexPath)
