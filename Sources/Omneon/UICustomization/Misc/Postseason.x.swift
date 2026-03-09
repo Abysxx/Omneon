17853d51ad7d2e2073509cbade69686cb0d4d629
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-03-09 09:58:48 -0500

    Update Postseason.x.swift

--- Sources/Omneon/UICustomization/Misc/Postseason.x.swift
+++ Sources/Omneon/UICustomization/Misc/Postseason.x.swift
@@ -12,7 +12,10 @@ class SPTPlayerTrack_OmneonHook_2: ClassHook<NSObject> {
   func metadata() -> [String: String] {
       var meta = orig.metadata()
       if(meta["title"] == "#BoxOnnaBack"){
+        meta["image_small_url"] = "https://i1.sndcdn.com/artworks-C9hhnPzL19C0iclt-NqJ4IQ-t1080x1080.jpg"
         meta["image_url"] = "https://i1.sndcdn.com/artworks-C9hhnPzL19C0iclt-NqJ4IQ-t1080x1080.jpg"
+        meta["image_large_url"] = "https://i1.sndcdn.com/artworks-C9hhnPzL19C0iclt-NqJ4IQ-t1080x1080.jpg"
+        meta["image_xlarge_url"] = "https://i1.sndcdn.com/artworks-C9hhnPzL19C0iclt-NqJ4IQ-t1080x1080.jpg"
       }
       return meta
   }
