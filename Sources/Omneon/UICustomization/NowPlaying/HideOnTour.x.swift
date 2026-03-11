86e4cc85d4928aa9c91444fdd27c43aa3ea97f04
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-03-11 09:50:12 -0500

    Create HideOnTour.x.swift

---
+++ Sources/Omneon/UICustomization/NowPlaying/HideOnTour.x.swift
@@ -0,0 +1,89 @@
+import Orion
+import UIKit
+import Foundation
+
+struct HideOnTour: HookGroup {}
+
+var hiddenIndexPath_5: IndexPath? = nil
+
+func scanAndHideOnTour(in collectionView: UICollectionView) {
+    for cell in collectionView.visibleCells {
+        if containsIdentifier(cell, identifier: "Components.UI.OnTourEventCard"),
+           let indexPath = collectionView.indexPath(for: cell) {
+            if hiddenIndexPath_5 != indexPath {
+                hiddenIndexPath_5 = indexPath
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
+                }
+            }
+            break
+        }
+    }
+}
+
+class HideOnTour_ViewControllerHook: ClassHook<UIViewController> {
+    typealias Group = HideOnTour
+    static let targetName = "NowPlaying_ScrollImpl.NowPlayingScrollViewController"
+
+    @objc(nowPlayingScrollViewModelWithDidLoadComponentsFor:withDifferentProviders:scrollEnabledValueChanged:)
+    func nowPlayingScrollViewModelWithDidLoadComponents(for arg1: AnyObject, withDifferentProviders arg2: Bool, scrollEnabledValueChanged arg3: Bool) {
+        orig.nowPlayingScrollViewModelWithDidLoadComponents(for: arg1, withDifferentProviders: arg2, scrollEnabledValueChanged: arg3)
+        guard let collectionView = target.value(forKey: "collectionView") as? UICollectionView else {
+            return
+        }
+        scanAndHideOnTour(in: collectionView)
+    }
+
+    @objc(nowPlayingScrollViewModelWithDidMoveToRelativeTrack:withDifferentProviders:scrollEnabledValueChanged:)
+    func nowPlayingScrollViewModelWithDidMoveToRelativeTrack(for arg1: AnyObject, withDifferentProviders arg2: Bool, scrollEnabledValueChanged arg3: Bool) {
+        orig.nowPlayingScrollViewModelWithDidMoveToRelativeTrack(for: arg1, withDifferentProviders: arg2, scrollEnabledValueChanged: arg3)
+        guard let collectionView = target.value(forKey: "collectionView") as? UICollectionView else {
+            return
+        }
+        hiddenIndexPath_5 = nil
+        scanAndHideOnTour(in: collectionView)
+    }
+}
+
+class HideOnTour_DelegateHook: ClassHook<NSObject> {
+    typealias Group = HideOnTour
+    static let targetName = "NowPlaying_ScrollImpl.ScrollCollectionViewManagerWithDynamicSizingImplementation"
+
+    @objc(collectionView:numberOfItemsInSection:)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
+        let adjusted = hiddenIndexPath_5 != nil ? count - 1 : count
+        return adjusted
+    }
+
+    @objc(collectionView:cellForItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let hidden = hiddenIndexPath_5 else {
+            // Haven't found the target yet, check this cell
+            let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
+            if containsIdentifier(cell, identifier: "Components.UI.OnTourEventCard") {
+                hiddenIndexPath_5 = indexPath
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
+                }
+            }
+            return cell
+        }
+        let adjustedIndexPath = indexPath.item >= hidden.item
+            ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
+            : indexPath
+        return orig.collectionView(collectionView, cellForItemAt: adjustedIndexPath)
+    }
+
+    @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        // Catch cells that had subviews added asynchronously
+        if hiddenIndexPath_5 == nil && containsIdentifier(cell, identifier: "Components.UI.OnTourEventCard") {
+            hiddenIndexPath_5 = indexPath
+            DispatchQueue.main.async {
+                collectionView.reloadData()
+            }
+        }
+        orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
+    }
+}
