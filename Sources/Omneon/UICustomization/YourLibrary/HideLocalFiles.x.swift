d7de56c3139203d255cd80d4ce3ce236c390deb7
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 09:08:33 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -8,16 +8,14 @@ class YourLibraryCollectionView_Hook: ClassHook<UICollectionView> {
     typealias Group = HideLocalFiles
     static let targetName = "YourLibrary_CommonKit.YourLibraryCollectionView"
   
-    func viewDidLayoutSubviews() {
-        orig.viewDidLayoutSubviews()
-        guard let collectionView = target.view.subviews.first(where: { $0 is UICollectionView }) as? UICollectionView else { return }
-        
-        if let target = collectionView.subviews[safe: 2]?
+    func layoutSubviews() {
+        orig.layoutSubviews()
+        if let view = target.subviews[safe: 2]?
             .subviews[safe: 0]?
             .subviews[safe: 0],
-            target.accessibilityIdentifier == "LocalFiles.Row.Library" {
-            target.removeFromSuperview()
-            NSLog("[Omneon] removed LocalFiles.Row.Library")
+            view.accessibilityIdentifier == "LocalFiles.Row.Library" {
+            view.isHidden = true
+            NSLog("[Omneon] hidden LocalFiles.Row.Library")
         }
     }
 }
