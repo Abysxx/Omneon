bbeaba4f95c3d8eaefafadfd8b2c14f77a596a12
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 10:06:59 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -35,6 +35,16 @@ class YourLibraryContentViewBinder_Hook: ClassHook<NSObject> {
 
     @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        NSLog("[Omneon] willDisplay cell at \(indexPath) subviews: \(cell.subviews.count)")
+        if let s2 = cell.subviews[safe: 2] {
+            NSLog("[Omneon] subviews[2] subviews: \(s2.subviews.count)")
+            if let s0 = s2.subviews[safe: 0] {
+                NSLog("[Omneon] subviews[2][0] subviews: \(s0.subviews.count)")
+                if let s00 = s0.subviews[safe: 0] {
+                    NSLog("[Omneon] subviews[2][0][0] accessibilityIdentifier: \(s00.accessibilityIdentifier ?? "nil")")
+                }
+            }
+        }
         if hiddenLocalFilesIndexPath == nil,
             let view = cell.subviews[safe: 2]?.subviews[safe: 0]?.subviews[safe: 0],
             view.accessibilityIdentifier == "LocalFiles.Row.Library" {
