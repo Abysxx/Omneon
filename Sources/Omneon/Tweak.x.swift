b2775b9c114d2b1a63658e826e33233f2ac3651e
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 08:47:47 -0500

    Update Tweak.x.swift

--- Sources/Omneon/Tweak.x.swift
+++ Sources/Omneon/Tweak.x.swift
@@ -75,6 +75,9 @@ struct Omneon: Tweak {
         if UserDefaults.forcePlaylist {
             ForcePlaylist().activate()
         }
+        if UserDefaults.hideLocalFiles {
+            HideLocalFiles().activate()
+        }
         //Misc
         if UserDefaults.fuxkPostseason {
             Postseason().activate()
