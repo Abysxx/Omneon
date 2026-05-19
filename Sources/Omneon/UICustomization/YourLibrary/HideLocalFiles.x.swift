d2d542543310a540b64007c17db63179ac8a42b6
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 11:01:02 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -4,29 +4,17 @@ import SwiftUI
 
 struct HideLocalFiles: HookGroup { }
 
-class YourLibraryCollectionView_Hook: ClassHook<UICollectionView> {
+class HideLocalFiles_DataSourceHook: ClassHook<NSObject> {
     typealias Group = HideLocalFiles
-    static let targetName = "YourLibrary_CommonKit.YourLibraryCollectionView"
-
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        for cell in target.visibleCells {
-            if let view = cell.subviews[safe: 2]?.subviews[safe: 0]?.subviews[safe: 0],
-               view.accessibilityIdentifier == "LocalFiles.Row.Library" {
-                cell.isHidden = true
-                cell.frame = .zero
-            }
-        }
-    }
+    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryContentViewBinder"
 
     @objc(collectionView:layout:sizeForItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, layout: AnyObject, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if let cell = target.cellForItem(at: indexPath),
+        if let cell = collectionView.cellForItem(at: indexPath),
            let view = cell.subviews[safe: 2]?.subviews[safe: 0]?.subviews[safe: 0],
            view.accessibilityIdentifier == "LocalFiles.Row.Library" {
             return .zero
         }
         return orig.collectionView(collectionView, layout: layout, sizeForItemAt: indexPath)
     }
-    
 }
