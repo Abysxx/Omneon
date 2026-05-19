32f4fa466c8895cfe0c833666fd297e1eba0df1b
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 14:06:04 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -3,21 +3,25 @@ import UIKit
 import SwiftUI
 
 struct HideLocalFiles: HookGroup { }
-var localFilesIndexPath: IndexPath? = nil
+var localFilesItem: Int? = nil
+var isScanning = false
 
 func scanAndRemoveLocalFiles(in collectionView: UICollectionView) {
+    guard !isScanning else { return }
+    isScanning = true
+    localFilesItem = nil
     for cell in collectionView.visibleCells {
         if containsIdentifier(cell, identifier: "LocalFiles.Row.Library"),
            let indexPath = collectionView.indexPath(for: cell) {
-            if localFilesIndexPath != indexPath {
-                localFilesIndexPath = indexPath
-                DispatchQueue.main.async {
-                    collectionView.reloadData()
-                }
+            localFilesItem = indexPath.item
+            DispatchQueue.main.async {
+                collectionView.reloadData()
+                isScanning = false
             }
-            break
+            return
         }
     }
+    isScanning = false
 }
 
 class HideLocalFiles_ControllerHook: ClassHook<UIViewController> {
@@ -26,7 +30,8 @@ class HideLocalFiles_ControllerHook: ClassHook<UIViewController> {
 
     func viewWillAppear(_ animated: Bool) {
         orig.viewWillAppear(animated)
-        localFilesIndexPath = nil
+        localFilesItem = nil
+        isScanning = false
     }
 
     func viewDidAppear(_ animated: Bool) {
@@ -50,20 +55,20 @@ class HideLocalFiles_DelegateHook: ClassHook<NSObject> {
     @objc(collectionView:numberOfItemsInSection:)
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
-        return localFilesIndexPath != nil ? count - 1 : count
+        return localFilesItem != nil ? count - 1 : count
     }
 
     @objc(collectionView:cellForItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let hidden = localFilesIndexPath else {
+        guard let hidden = localFilesItem else {
             let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
             if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") {
-                localFilesIndexPath = indexPath
+                localFilesItem = indexPath.item
                 DispatchQueue.main.async { collectionView.reloadData() }
             }
             return cell
         }
-        let adjusted = indexPath.item >= hidden.item
+        let adjusted = indexPath.item >= hidden
             ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
             : indexPath
         return orig.collectionView(collectionView, cellForItemAt: adjusted)
@@ -71,9 +76,11 @@ class HideLocalFiles_DelegateHook: ClassHook<NSObject> {
 
     @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") && localFilesIndexPath != indexPath {
-            localFilesIndexPath = indexPath
-            DispatchQueue.main.async { collectionView.reloadData() }
+        if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") {
+            if localFilesItem != indexPath.item {
+                localFilesItem = indexPath.item
+                DispatchQueue.main.async { collectionView.reloadData() }
+            }
         }
         orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
     }
