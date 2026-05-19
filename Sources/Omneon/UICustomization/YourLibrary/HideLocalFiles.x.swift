77fcbeca409c7823f660ea08432d93333c75b5a8
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 12:08:23 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -12,28 +12,27 @@ class HideLocalFiles_DataSourceHook: ClassHook<NSObject> {
     @objc(collectionView:numberOfItemsInSection:)
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
+        // scan all items to find localFiles index
+        for i in 0..<count {
+            let ip = IndexPath(item: i, section: section)
+            let cell = orig.collectionView(collectionView, cellForItemAt: ip)
+            if cell.reuseIdentifier == "YourLibraryListItemBinderIdentifier.localFilesRow.none" {
+                localFilesIndexPath = ip
+                break
+            }
+        }
         return localFilesIndexPath != nil ? count - 1 : count
     }
 
     @objc(collectionView:cellForItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
-        if cell.reuseIdentifier == "YourLibraryListItemBinderIdentifier.localFilesRow.none" {
-            localFilesIndexPath = indexPath
-            DispatchQueue.main.async { collectionView.reloadData() }
+        guard let hidden = localFilesIndexPath else {
+            return orig.collectionView(collectionView, cellForItemAt: indexPath)
         }
-        return cell
-    }
-
-    @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if cell.reuseIdentifier == "YourLibraryListItemBinderIdentifier.localFilesRow.none" {
-            if localFilesIndexPath != indexPath {
-                localFilesIndexPath = indexPath
-                DispatchQueue.main.async { collectionView.reloadData() }
-            }
-        }
-        orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
+        let adjusted = indexPath.item >= hidden.item
+            ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
+            : indexPath
+        return orig.collectionView(collectionView, cellForItemAt: adjusted)
     }
 }
 
