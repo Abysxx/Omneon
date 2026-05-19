479f1bf9b787b1e90b5243c26c6102cba7f62f50
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 13:43:17 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -1,7 +1,8 @@
 import Orion
 import UIKit
-import Foundation
+import SwiftUI
 
+struct HideLocalFiles: HookGroup { }
 var localFilesIndexPath: IndexPath? = nil
 
 func scanAndRemoveLocalFiles(in collectionView: UICollectionView) {
@@ -19,15 +20,17 @@ func scanAndRemoveLocalFiles(in collectionView: UICollectionView) {
     }
 }
 
-struct HideLocalFiles: HookGroup {}
-
 class HideLocalFiles_ControllerHook: ClassHook<UIViewController> {
     typealias Group = HideLocalFiles
     static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryViewController"
-    
 
-    func viewDidLayoutSubviews() {
-        orig.viewDidLayoutSubviews()
+    func viewWillAppear(_ animated: Bool) {
+        orig.viewWillAppear(animated)
+        localFilesIndexPath = nil
+    }
+
+    func viewDidAppear(_ animated: Bool) {
+        orig.viewDidAppear(animated)
         let view = self.target.view
         guard
             let s0 = view?.subviews[safe: 0],
@@ -36,49 +39,41 @@ class HideLocalFiles_ControllerHook: ClassHook<UIViewController> {
             let s3 = s2.subviews[safe: 0],
             let collectionView = s3.subviews[safe: 0] as? UICollectionView
         else { return }
-        localFilesIndexPath = nil
-        scanAndHideCredits(in: collectionView)
-    }        
+        scanAndRemoveLocalFiles(in: collectionView)
+    }
 }
 
 class HideLocalFiles_DelegateHook: ClassHook<NSObject> {
     typealias Group = HideLocalFiles
     static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryContentViewBinder"
-    
+
     @objc(collectionView:numberOfItemsInSection:)
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
-        let adjusted = localFilesIndexPath != nil ? count - 1 : count
-        return adjusted
+        return localFilesIndexPath != nil ? count - 1 : count
     }
 
     @objc(collectionView:cellForItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let hidden = localFilesIndexPath else {
-            // Haven't found the target yet, check this cell
             let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
             if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") {
                 localFilesIndexPath = indexPath
-                DispatchQueue.main.async {
-                    collectionView.reloadData()
-                }
+                DispatchQueue.main.async { collectionView.reloadData() }
             }
             return cell
         }
-        let adjustedIndexPath = indexPath.item >= hidden.item
+        let adjusted = indexPath.item >= hidden.item
             ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
             : indexPath
-        return orig.collectionView(collectionView, cellForItemAt: adjustedIndexPath)
+        return orig.collectionView(collectionView, cellForItemAt: adjusted)
     }
 
     @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        // Catch cells that had subviews added asynchronously
-        if localFilesIndexPath == nil && containsIdentifier(cell, identifier: "LocalFiles.Row.Library") {
+        if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") && localFilesIndexPath != indexPath {
             localFilesIndexPath = indexPath
-            DispatchQueue.main.async {
-                collectionView.reloadData()
-            }
+            DispatchQueue.main.async { collectionView.reloadData() }
         }
         orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
     }
