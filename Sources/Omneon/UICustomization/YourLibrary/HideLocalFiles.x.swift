0797f820a60db6840e9d353bbd457cd10bb75f58
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-19 11:29:44 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -3,21 +3,47 @@ import UIKit
 import SwiftUI
 
 struct HideLocalFiles: HookGroup { }
+var localFilesIndexPath: IndexPath? = nil
 
-class YourLibraryCompositionalLayout_Hook: ClassHook<UICollectionViewLayout> {
+class YourLibraryCollectionView_Hook: ClassHook<UICollectionView> {
     typealias Group = HideLocalFiles
-    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryCompositionalLayout"
+    static let targetName = "YourLibrary_CommonKit.YourLibraryCollectionView"
 
-    @objc(layoutAttributesForElementsInRect:)
-    func layoutAttributesForElements(in rect: CGRect) -> AnyObject? {
-        guard let attrs = orig.layoutAttributesForElements(in: rect) as? [UICollectionViewLayoutAttributes] else { return nil }
-        for attr in attrs {
-            if let cell = target.collectionView?.cellForItem(at: attr.indexPath),
-               cell.reuseIdentifier == "YourLibraryListItemBinderIdentifier.localFilesRow.none" {
-                attr.frame = .zero
-                attr.isHidden = true
+    func layoutSubviews() {
+        orig.layoutSubviews()
+        for cell in target.visibleCells {
+            if cell.reuseIdentifier == "YourLibraryListItemBinderIdentifier.localFilesRow.none",
+               let indexPath = target.indexPath(for: cell) {
+                if localFilesIndexPath != indexPath {
+                    localFilesIndexPath = indexPath
+                    target.performBatchUpdates {
+                        target.deleteItems(at: [indexPath])
+                    }
+                }
+                break
             }
         }
-        return attrs as AnyObject
+    }
+}
+
+class HideLocalFiles_DataSourceHook: ClassHook<NSObject> {
+    typealias Group = HideLocalFiles
+    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryContentViewBinder"
+
+    @objc(collectionView:numberOfItemsInSection:)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
+        return localFilesIndexPath != nil ? count - 1 : count
+    }
+
+    @objc(collectionView:cellForItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let hidden = localFilesIndexPath else {
+            return orig.collectionView(collectionView, cellForItemAt: indexPath)
+        }
+        let adjusted = indexPath.item >= hidden.item
+            ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
+            : indexPath
+        return orig.collectionView(collectionView, cellForItemAt: adjusted)
     }
 }
