21031bf7dbd65afdaac041ae504e4918d517a724
Abyss <159920611+Abysxx@users.noreply.github.com>
2026-05-22 08:26:50 -0500

    Update HideLocalFiles.x.swift

--- Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
+++ Sources/Omneon/UICustomization/YourLibrary/HideLocalFiles.x.swift
@@ -1,290 +1,82 @@
+// Dissabled due to bug of duplicating playlists, and shifted indexs
+//
 import Orion
 import UIKit
 import SwiftUI
-import ObjectiveC.runtime
-
-// MARK: - Helpers
-
-func logIvars(_ obj: AnyObject, className: String? = nil) {
-    let actualClass: AnyClass = className.flatMap(NSClassFromString) ?? type(of: obj)
-
-    NSLog("========== IVARS FOR \(NSStringFromClass(actualClass)) ==========")
-
-    var count: UInt32 = 0
-
-    guard let ivars = class_copyIvarList(actualClass, &count) else {
-        NSLog("[Omneon] No ivars found")
-        return
-    }
-
-    defer {
-        free(ivars)
-    }
-
-    for i in 0..<Int(count) {
-        let ivar = ivars[i]
-
-        guard let name = ivar_getName(ivar) else { continue }
-
-        let ivarName = String(cString: name)
-
-        if let value = object_getIvar(obj, ivar) {
-            NSLog("[Omneon] \(ivarName): \(value)")
-        } else {
-            NSLog("[Omneon] \(ivarName): nil")
-        }
-    }
-}
-
-func logProperties(_ obj: AnyObject, className: String? = nil) {
-    let actualClass: AnyClass = className.flatMap(NSClassFromString) ?? type(of: obj)
-
-    NSLog("========== PROPERTIES FOR \(NSStringFromClass(actualClass)) ==========")
-
-    var count: UInt32 = 0
-
-    guard let properties = class_copyPropertyList(actualClass, &count) else {
-        NSLog("[Omneon] No properties found")
-        return
-    }
-
-    defer {
-        free(properties)
-    }
-
-    for i in 0..<Int(count) {
-        let property = properties[i]
-
-        // FIX: Remove 'guard let' because property_getName returns a non-optional pointer
-        let name = property_getName(property)
-
-        let propName = String(cString: name)
-
-        if let value = obj.value(forKey: propName) {
-            NSLog("[Omneon] \(propName): \(value)")
-        } else {
-            NSLog("[Omneon] \(propName): nil")
-        }
-    }
-}
-
-func logEverything(_ obj: AnyObject, name: String? = nil) {
-    let cls = name ?? NSStringFromClass(type(of: obj))
-
-    NSLog("===================================================")
-    NSLog("[Omneon] LOGGING OBJECT: \(cls)")
-    NSLog("[Omneon] INSTANCE: \(obj)")
-    NSLog("===================================================")
-
-    logProperties(obj)
-    logIvars(obj)
-}
-
-// MARK: - Hook Groups
 
 struct HideLocalFiles: HookGroup { }
-struct YourLibraryLoggerHooks: HookGroup { }
+var localFilesIndexPath: IndexPath? = nil
 
-// MARK: - Sort Rule
-
-class SortRuleHook: ClassHook<NSObject> {
-    typealias Group = HideLocalFiles
-    static let targetName = "_TtCV21YourLibrary_CommonKit31YourLibrarySortOptionPickerData8SortRule"
-
-    func identifier() -> AnyObject {
-        let result = orig.identifier()
-        NSLog("[Omneon] SortRule.identifier -> \(result)")
-        return result
-    }
-
-    func title() -> AnyObject {
-        let result = orig.title()
-        NSLog("[Omneon] SortRule.title -> \(result)")
-        return result
-    }
-
-    func ascendingOrder() -> Bool {
-        let result = orig.ascendingOrder()
-        NSLog("[Omneon] SortRule.ascendingOrder -> \(result)")
-        return result
-    }
-
-    func isEqual(_ other: AnyObject?) -> Bool {
-        NSLog("[Omneon] SortRule.isEqual: \(String(describing: other))")
-        return orig.isEqual(other)
-    }
-    
-    @objc(init)
-    func myInit() -> Target {
-        let obj = orig.myInit() // Safely calls the original Obj-C -init
-        NSLog("[Omneon] SortRule.init")
-        if let o = obj as? AnyObject { logEverything(o) }
-        return obj
-    }
-}
-
-// MARK: - Sorting Filtering Picker
-
-class SortingFilteringPickerHook: ClassHook<NSObject> {
-    typealias Group = HideLocalFiles
-    static let targetName = "YourLibrary_CommonKit.YourLibrarySortingFilteringPickerController"
-
-    @objc(sortingFilteringPicker:selectedSortRule:)
-    func sortingFilteringPicker(_ picker: AnyObject, selectedSortRule rule: AnyObject) {
-        NSLog("===================================================")
-        NSLog("[Omneon] selectedSortRule")
-        NSLog("===================================================")
-        NSLog("[Omneon] rule: \(rule)")
-        NSLog("[Omneon] class: \(NSStringFromClass(type(of: rule)))")
-
-        logEverything(rule)
-        orig.sortingFilteringPicker(picker, selectedSortRule: rule)
-    }
-
-    @objc(sortingFilteringPicker:selectedFilterRule:)
-    func sortingFilteringPicker(_ picker: AnyObject, selectedFilterRule rule: AnyObject) {
-        NSLog("===================================================")
-        NSLog("[Omneon] selectedFilterRule")
-        NSLog("===================================================")
-        NSLog("[Omneon] rule: \(rule)")
-        NSLog("[Omneon] class: \(NSStringFromClass(type(of: rule)))")
-
-        logEverything(rule)
-        orig.sortingFilteringPicker(picker, selectedFilterRule: rule)
-    }
-
-    @objc(sortingFilteringPicker:deselectedFilterRule:)
-    func sortingFilteringPicker(_ picker: AnyObject, deselectedFilterRule rule: AnyObject) {
-        NSLog("===================================================")
-        NSLog("[Omneon] deselectedFilterRule")
-        NSLog("===================================================")
-        NSLog("[Omneon] rule: \(rule)")
-        NSLog("[Omneon] class: \(NSStringFromClass(type(of: rule)))")
-
-        logEverything(rule)
-        orig.sortingFilteringPicker(picker, deselectedFilterRule: rule)
-    }
-
-    @objc(didCancelSortingFilteringPicker:reason:)
-    func didCancelSortingFilteringPicker(_ picker: AnyObject, reason: UInt) {
-        NSLog("[Omneon] didCancelSortingFilteringPicker reason: \(reason)")
-        orig.didCancelSortingFilteringPicker(picker, reason: reason)
-    }
-}
-
-// MARK: - Header Content Filters View
-
-class HeaderContentFiltersViewHook: ClassHook<UIView> {
-    typealias Group = HideLocalFiles
-    static let targetName = "YourLibrary_CommonKit.YourLibraryHeaderContentFiltersView"
-
-    func initWithFrame(_ frame: CGRect) -> Target {
-        let obj = orig.initWithFrame(frame)
-        NSLog("[Omneon] HeaderContentFiltersView.initWithFrame")
-
-        if let o = obj as? AnyObject {
-            logEverything(o)
+func scanAndRemoveLocalFiles(in collectionView: UICollectionView) {
+    for cell in collectionView.visibleCells {
+        if containsIdentifier(cell, identifier: "LocalFiles.Row.Library"),
+           let indexPath = collectionView.indexPath(for: cell) {
+            if localFilesIndexPath != indexPath {
+                localFilesIndexPath = indexPath
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
+                }
+            }
+            break
         }
-
-        return obj
-    }
-
-    func initWithCoder(_ coder: NSCoder) -> Target {
-        let obj = orig.initWithCoder(coder)
-        NSLog("[Omneon] HeaderContentFiltersView.initWithCoder")
-
-        if let o = obj as? AnyObject {
-            logEverything(o)
-        }
-
-        return obj
     }
 }
 
-// MARK: - Filter Chips Provider Config
-
-class FilterChipsProviderConfigHook: ClassHook<NSObject> {
+class HideLocalFiles_ControllerHook: ClassHook<UIViewController> {
     typealias Group = HideLocalFiles
-    static let targetName = "CollectionSongs_PlaylistImpl.CSFilterChipsProviderConfig"
+    static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryViewController"
 
-    @objc(filterDataLoaderLoaded:filters:)
-    func filterDataLoaderLoaded(_ loader: AnyObject, filters: AnyObject) {
-        NSLog("[Omneon] filterDataLoaderLoaded")
-        NSLog("[Omneon] loader: \(loader)")
-        NSLog("[Omneon] filters: \(filters)")
-
-        logEverything(filters)
-        orig.filterDataLoaderLoaded(loader, filters: filters)
+    func viewWillAppear(_ animated: Bool) {
+        orig.viewWillAppear(animated)
+        localFilesIndexPath = nil
     }
 
-    @objc(filterDataLoaderError:error:)
-    func filterDataLoaderError(_ loader: AnyObject, error: AnyObject) {
-        NSLog("[Omneon] filterDataLoaderError")
-        NSLog("[Omneon] loader: \(loader)")
-        NSLog("[Omneon] error: \(error)")
-
-        orig.filterDataLoaderError(loader, error: error)
-    }
-    
-    @objc(init)
-    func myInit() -> Target {
-        let obj = orig.myInit()
-        NSLog("[Omneon] CSFilterChipsProviderConfig.init")
-        if let o = obj as? AnyObject { logEverything(o) }
-        return obj
+    func viewDidAppear(_ animated: Bool) {
+        orig.viewDidAppear(animated)
+        let view = self.target.view
+        guard
+            let s0 = view?.subviews[safe: 0],
+            let s1 = s0.subviews[safe: 0],
+            let s2 = s1.subviews[safe: 0],
+            let s3 = s2.subviews[safe: 0],
+            let collectionView = s3.subviews[safe: 0] as? UICollectionView
+        else { return }
+        scanAndRemoveLocalFiles(in: collectionView)
     }
 }
 
-// MARK: - Songs Filter Impl
-
-class SongsFilterImplHook: ClassHook<NSObject> {
-    typealias Group = YourLibraryLoggerHooks
-    static let targetName = "CollectionSongs_SongsImpl.SongsFilterImpl"
-
-    func query() -> AnyObject {
-        let result = orig.query()
-        NSLog("[Omneon] SongsFilterImpl.query -> \(result)")
-        return result
-    }
-
-    @objc(setQuery:)
-    func setQuery(_ query: AnyObject) {
-        NSLog("[Omneon] SongsFilterImpl.setQuery -> \(query)")
-        orig.setQuery(query)
-    }
-
-    func title() -> AnyObject {
-        let result = orig.title()
-        NSLog("[Omneon] SongsFilterImpl.title -> \(result)")
-        return result
-    }
-
-    @objc(setTitle:)
-    func setTitle(_ title: AnyObject) {
-        NSLog("[Omneon] SongsFilterImpl.setTitle -> \(title)")
-        orig.setTitle(title)
-    }
-
-    @objc(init)
-    func myInit() -> Target {
-        let obj = orig.myInit()
-        NSLog("[Omneon] SongsFilterImpl.init")
-        if let o = obj as? AnyObject { logEverything(o) }
-        return obj
-    }
-}
-
-// MARK: - Content View Binder
-
-class ContentViewBinderHook: ClassHook<NSObject> {
+class HideLocalFiles_DelegateHook: ClassHook<NSObject> {
     typealias Group = HideLocalFiles
     static let targetName = "YourLibrary_YourLibraryXImpl.YourLibraryContentViewBinder"
-    
-    @objc(init)
-    func myInit() -> Target {
-        let obj = orig.myInit()
-        NSLog("[Omneon] YourLibraryContentViewBinder.init")
-        if let o = obj as? AnyObject { logEverything(o) }
-        return obj
+
+    @objc(collectionView:numberOfItemsInSection:)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let count = orig.collectionView(collectionView, numberOfItemsInSection: section)
+        return localFilesIndexPath != nil ? count - 1 : count
+    }
+
+    @objc(collectionView:cellForItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let hidden = localFilesIndexPath else {
+            let cell = orig.collectionView(collectionView, cellForItemAt: indexPath)
+            if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") {
+                localFilesIndexPath = indexPath
+                DispatchQueue.main.async { collectionView.reloadData() }
+            }
+            return cell
+        }
+        let adjusted = indexPath.item >= hidden.item
+            ? IndexPath(item: indexPath.item + 1, section: indexPath.section)
+            : indexPath
+        return orig.collectionView(collectionView, cellForItemAt: adjusted)
+    }
+
+    @objc(collectionView:willDisplayCell:forItemAtIndexPath:)
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if containsIdentifier(cell, identifier: "LocalFiles.Row.Library") && localFilesIndexPath != indexPath {
+            localFilesIndexPath = indexPath
+            DispatchQueue.main.async { collectionView.reloadData() }
+        }
+        orig.collectionView(collectionView, willDisplay: cell, forItemAt: indexPath)
     }
 }
